ac4f8d0064324a95f8728ca17613b90756db35fa
gbemiokeowo <gbemiokeowo2016@gmail.com>
2025-09-15 16:12:13 +0100

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -50,7 +50,7 @@ interface printTeacherFunction {
 }
 
 // Implementation of the function
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
+function printTeacher: printTeacherFunction = ({ firstName, lastName }) => {
  return `${firstName}. ${lastName}`;
 };
 
